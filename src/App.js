eed3cb04449121dc23cb95041dbbb6a4a02b6571
Colin Swaney <colinswaney@princeton.edu>
2023-04-17 20:10:28 -0400

    Log messages

--- src/App.js
+++ src/App.js
@@ -65,10 +65,11 @@ function App() {
         body: JSON.stringify([...messages, userMessage]),
       });
       const newMessages = await response.json();
+      console.log(newMessages[newMessages.length - 1]);
       const index = newMessages[newMessages.length - 1].content.search("[END]");
       if (index > -1) {
-        console.log(`surveyFinished=${surveyFinished} (${newMessages[newMessages.length - 1]})`);
         setSurveyFinished(true);
+        console.log(`surveyFinished=${surveyFinished}`);
         const finalMessage = {
           role: newMessages[newMessages.length - 1].role,
           content: newMessages[newMessages.length - 1].content.slice(0, index)
